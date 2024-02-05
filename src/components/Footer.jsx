ef2bd56bca426b2097840acb2988ea52d188a51e
amansagar0403 <as624@snu.edu.in>
2024-02-05 11:14:15 +0530

    complete project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 function Footer() {
-  const date = new Date();
-  const year = date.getFullYear();
+  const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright © {year}</p>
+      <p>Copyright ⓒ {year}</p>
     </footer>
   );
 }
